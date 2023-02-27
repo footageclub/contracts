4b5e686d8b4af30f9e7a563c31919af54c7b145d
footageclub <info@footage.club>
2023-02-27 14:22:22 +0800

    change dir

---
+++ contracts-upgradeable/hardhat.config.ts
@@ -0,0 +1,65 @@
+import fs from "fs"
+
+import "dotenv/config";
+import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-preprocessor";
+
+// Configure remappings.
+// https://book.getfoundry.sh/config/hardhat
+// Re-run `forge remappings > remappings.txt`
+// every time you modify libraries in Foundry.
+function getRemappings() {
+    return fs
+      .readFileSync("remappings.txt", "utf8")
+      .split("\n")
+      .filter(Boolean) // remove empty lines
+      .map((line: string) => line.trim().split("="));
+  }
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    goerli: {
+      url: process.env.RPC_URL_GOERLI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
+
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/ from "/i)) {
+          getRemappings().forEach(([find, replace]: string[]) => {
+            console.log("----d-----", find, replace, line.match(find));
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+  paths: { sources: "./footage" },
+};
