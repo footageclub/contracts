46399ae08d2a37b0a64cf48ef5cc1798b43d3537
435420057 <435420057@qq.com>
2023-02-27 10:42:36 +0800

    remove old

--- migrations/5_deploy_club_erc20.js
+++
@@ -1,7 +0,0 @@
-const ClubERC20 = artifacts.require("ClubERC20");
-
-module.exports = async function(deployer) {    
-    deployer.deploy(ClubERC20, "Club", "ERC20", 68000).then(function(instance){
-        console.log('ClubERC20 Deployed', instance.address);
-    });
-};
--- migrations/6_deploy_clubnft.js
+++
@@ -1,7 +0,0 @@
-const ClubNFT = artifacts.require("ClubNFT");
-
-module.exports = async function(deployer) {    
-    deployer.deploy(ClubNFT, "0x4cb561273a2757d33f0d044d5489236d5bbd9e36", "0x4cb561273a2757d33f0d044d5489236d5bbd9e36", 250, 250, "https://www.footage.club/{id}.json").then(function(instance){
-        console.log('ClubNFT Deployed', instance.address);
-    });
-};
